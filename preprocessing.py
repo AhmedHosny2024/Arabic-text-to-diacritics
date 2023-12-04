f3414324c687834c9e8e3d8ba46024b3b720ecdd
ZiadSheriif <zsherif308@gmail.com>
2023-12-04 21:24:22 +0200

    feat: Add more regex

--- preprocessing.py
+++ preprocessing.py
@@ -2,56 +2,58 @@ import re
 import os
 
 
-def preprocess_text(text):
-    # Remove non-Arabic characters
-    text = re.sub(r"[^\u0600-\u06FF\s]+", "", text)
+class TextProcessor:
+    def __init__(self, input_folder, output_folder):
+        self.input_folder = input_folder
+        self.output_folder = output_folder
 
-    # Normalize white spaces
-    text = re.sub(r"\s+", " ", text)
+    def preprocess_text(self, text):
+        # remove non-Arabic characters
+        text = re.sub(r"[^\u0600-\u06FF\s]+", "", text)
+        # remove extra spaces
+        text = re.sub(r"\s+", " ", text)
+        # Remove numbers
+        text = re.sub(r"\d+", "", text)
+        # remove html tags
+        text = re.sub(r"<.*?>", "", text)
+        # remove urls
+        text = re.sub(r"http\S+", "", text)
+        # remove emails
+        text = re.sub(r"\S+@\S+", "", text)
+        # remove hashtags
+        text = re.sub(r"#\S+", "", text)
+        # remove mentions
+        text = re.sub(r"@\S+", "", text)
+        # remove brackets
+        text = re.sub(r"\[.*?\]", "", text)
+        return text
 
-    # Remove numbers
-    text = re.sub(r"\d+", "", text)
+    def read_text(self, file_name):
+        with open(f"{self.input_folder}/{file_name}", "r", encoding="utf-8") as file:
+            return file.read()
 
-    # remove html tags
-    text = re.sub(r"<.*?>", "", text)
+    def write_to_file(self, file_name, text):
+        output_file_path = f"{self.output_folder}/{file_name}"
+        output_folder_name = os.path.dirname(output_file_path)
 
-    # remove urls
-    text = re.sub(r"http\S+", "", text)
+        if not os.path.exists(output_folder_name):
+            os.makedirs(output_folder_name)
 
-    # remove emails
-    text = re.sub(r"\S+@\S+", "", text)
+        with open(output_file_path, "w", encoding="utf-8") as file:
+            lines = [
+                text[i : i + 200] for i in range(0, len(text), 200)
+            ]  # max 200 characters per line
+            file.write("\n".join(lines))
+        # print(f"Text written to {output_file_path}")
 
-    return text
-
-
-def read_text(file_path):
-    with open(file_path, "r") as file:
-        return file.read()
-
-
-def write_to_file(output_file_path, text):
-    # Extract the directory path from the file path
-    output_folder_name = os.path.dirname(output_file_path)
-
-    if not os.path.exists(output_folder_name):
-        os.makedirs(output_folder_name)
-
-    
-    with open(output_file_path, "w", encoding="utf-8") as file:
-        lines = [text[i:i+200] for i in range(0, len(text), 120)] # max 200 characters per line
-        file.write('\n'.join(lines))
-    print(f"Text written to {output_file_path}")
+    def process_file(self, input_file_name, output_file_name):
+        text = self.read_text(input_file_name)
+        print("Before preprocessing data:", len(text))
+        clean_text = self.preprocess_text(text)
+        print("After preprocessing data:", len(clean_text))
+        self.write_to_file(output_file_name, clean_text)
 
 
-folder_name = "dataset"
-output_folder_name = "clean dataset"
-
-text = read_text(f"{folder_name}/train.txt")[:10000]
-clean_text = preprocess_text(text)
-
-
-output_file_path = f"{output_folder_name}/clean_train.txt"
-
-write_to_file(output_file_path, clean_text)
-
-print(clean_text[::-1])
+# Usage
+processor = TextProcessor("dataset", "clean dataset")
+processor.process_file("train.txt", "clean_train.txt")
