bb46e070d0a63dc41cd08bde6cca2b80bcebe005
ZiadSheriif <zsherif308@gmail.com>
2023-12-04 22:14:56 +0200

    feat: Add structure of feature extraction

---
+++ feature_extraction.py
@@ -0,0 +1,19 @@
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+
+class ArabicTextFeatures:
+    def __init__(self, text):
+        self.text = text
+
+    def bag_of_words(self):
+        vectorizer = CountVectorizer()
+        bow = vectorizer.fit_transform([self.text]) 
+        return bow.toarray()
+
+    def tfidf(self):
+        vectorizer = TfidfVectorizer()
+        tfidf = vectorizer.fit_transform([self.text]) 
+        return tfidf.toarray()
+
+    def char_ngrams(self, n=2):
+        ngrams = [self.text[i : i + n] for i in range(len(self.text) - n + 1)] 
+        return ngrams
