faa35532115e7ddfda7908865750ba6fd9d1b9f1
AhmedHosny2024 <eng.ahmedhosny2024@gmail.com>
2023-12-11 01:50:22 +0200

    fix : remove numbers in data cleaniing

--- DataCleaning.py
+++ DataCleaning.py
@@ -16,7 +16,6 @@ class DataCleaning:
         # All characters except the Arabic letters, diacritics, and punctuation
         with open('./pickles/ARABIC_LETTERS_LIST.pickle', 'rb') as file:
             ARABIC_LETTERS_LIST = pkl.load(file)
-
         with open('./pickles/DIACRITICS_LIST.pickle', 'rb') as file:
             DIACRITICS_LIST = pkl.load(file)
         # Remove HTML tags using BeutifulSoup
@@ -24,7 +23,7 @@ class DataCleaning:
         # Remove URLs 
         text = re.sub(r"http[s|S]\S+", "", text,flags=re.MULTILINE)
         text = re.sub(r"www\S+", "", text,flags=re.MULTILINE)
-        # Remove English letters
+        # Remove English letters 
         text = re.sub(r"[A-Za-z]+", "", text,flags=re.MULTILINE)
         # Remove Kashida Arabic character
         text = re.sub(r"\u0640", "", text,flags=re.MULTILINE)
@@ -34,9 +33,10 @@ class DataCleaning:
         text = re.sub(r"\s+", " ", text,flags=re.MULTILINE)
         # removes SHIFT+J Arabic character
         text = re.sub(r"\u0691", "", text,flags=re.MULTILINE)
-        # Remove diacritics
-        for diacritic in DIACRITICS_LIST:
-            text = text.replace(diacritic, "")
+        # remove english numbers
+        text = re.sub(r"[0-9]+", "", text,flags=re.MULTILINE)
+        # remove arabic numbers
+        text = re.sub(r"[٠-٩]+", "", text,flags=re.MULTILINE)
         # taken from the original repo code to fixes diacritics positions and remove unneeded or misplaced ones 
         def fix_diacritics(content):
             content = re.sub(r'اً', 'ًا', content)
@@ -49,6 +49,9 @@ class DataCleaning:
             # remove diacritics from the beginning of the word
             content = re.sub(r'(?P<diac>[' + ''.join(DIACRITICS_LIST[:-1]) + '])[' + ''.join(DIACRITICS_LIST) + ']+', r'\g<diac>', content)
             return content
+        # Remove diacritics
+        for diacritic in DIACRITICS_LIST:
+            text = text.replace(diacritic, "")
         text = fix_diacritics(text)
         return text
 
@@ -71,7 +74,6 @@ class DataCleaning:
         clean_text = self.preprocess_text(text)
         print("After preprocessing data:", len(clean_text))
         self.write_to_file(self.output_folder,output_path, clean_text)
-
 
 processor = DataCleaning("Dataset", "Data Cleaning")
 processor.process_file("train.txt", "DataCleaning.txt")
